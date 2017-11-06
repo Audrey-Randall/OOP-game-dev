f1a13053bf78c74eb2bc2e4a89609b5cf4d6b303
leopardGeckos <pgutenko@gmail.com>
2017-11-06 02:07:01 -0700

    Fix potential line-end bug

--- src/main/Tilemap.java
+++ src/main/Tilemap.java
@@ -8,14 +8,15 @@ public class Tilemap {
     public final int TILE_SIZE = 32;
 
     public Tilemap() {
-        String map = FileIO.getString("/res/files/level1.txt");
+        String file = FileIO.getString("/res/files/level1.txt");
+        String[] map = file.split("\n");
 
         int width = 60;
         tiles = new int[width][15];
         for (int x = 0; x < tiles.length; x++)
             for (int y = 0; y < tiles[0].length; y++)
             {
-                tiles[x][y] = (int)(map.charAt(x+y*(width+1)))-48;
+                tiles[x][y] = (int)(map[y].charAt(x))-48;
             }
     }
 }
