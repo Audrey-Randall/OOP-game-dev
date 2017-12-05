23e7d5712458e102017ecc41863a732cfc8ef501
Audrey Randall <audrey.randall@colorado.edu>
2017-12-04 20:41:37 -0700

    This code works with my configured SQL Server instance. Weird things: it listens on port 60776, SQL Server hides password lengths (which makes it look like it's arbitrarily changing them), and a lot of configuration has to happen in SQL Management Studio for anything to connect.

--- src/menu/behavior/LeaderboardBehavior.java
+++ src/menu/behavior/LeaderboardBehavior.java
@@ -18,12 +18,18 @@ public class LeaderboardBehavior extends MenuBehavior {
     
     private String[] getScores() {
     	try {
-    		String host = "Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;";
-    		String conUrl = "jdbc:sqlserver://localhost:60776; databaseName=players; user=sa; password=Password123;";
+    		/*String conUrl = "jdbc:sqlserver://localhost:60776; databaseName=players; user=TPD;";
         	String username = "";
         	String password = "";
-    		Connection dbCon = DriverManager.getConnection(conUrl);
-    	} catch (SQLException e) {
+    		Connection dbCon = DriverManager.getConnection(conUrl);*/
+    		String userName = "Audacity";
+    		String password = "12345";
+
+    		String url = "jdbc:sqlserver://AUDREY-PC\\SQLEXPRESS:60776;databaseName=players;";
+
+    		//Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    		Connection conn = DriverManager.getConnection(url, userName, password);
+    	} catch (Exception e) {
     		System.out.println("?!!");
     		System.out.println(e.getMessage());
     	}
